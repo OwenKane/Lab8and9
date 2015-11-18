21bba60891f4dcc60e6bf2b587cb049b3ea5d5b1
OwenKane <owen.a.kane@gmail.com>
2015-11-18 15:56:16 +0000

    Updated files to the same version I am demoing

--- script.sh
+++ script.sh
@@ -10,28 +10,28 @@ function restart {
 selection=
 until [ "$selection" = "0" ]; do
 	printf "\nSelect from the menu: \n\n"
-	printf "  1) VM Connection Test\n"
-	printf "  2) List all Containers\n"
-	printf "  3) List Running Containers\n"
-	printf "  4) Inspect a Container\n"
-	printf "  5) Delete a Container\n"
-	printf "  6) Delete all Containers\n"
-	printf "  7) Create a Container from an Image\n"
-	printf "  8) Restart a Container\n"
-	printf "  9) Stop a Container\n"
-	printf " 10) Show Logs from a Container\n"
-	printf " 11) List all Images\n"
-	printf " 12) Delete an Image\n"
-	printf " 13) Delete all Images\n"
-	printf " 14) TAG an Image\n"
-	printf " 15) Create Docker Images from Local Dockerfile\n"
-	printf " \n 99) Exit\n"
+	printf "Test 1 - VM Connection Test\n"
+	printf "Test 2 - List all Containers\n"
+	printf "Test 3 - List Running Containers\n"
+	printf "Test 4 - Inspect a Container\n"
+	printf "Test 5 - Delete a Container\n"
+	printf "Test 6 - Delete all Containers\n"
+	printf "Test 7 - Create a Container from an Image\n"
+	printf "Test 8 - Restart a Container\n"
+	printf "Test 9 - Stop a Container\n"
+	printf "Test 10 - Show Logs from a Container\n"
+	printf "Test 11 - List all Images\n"
+	printf "Test 12 - Delete an Image\n"
+	printf "Test 13 - Delete all Images\n"
+	printf "Test 14 - TAG an Image\n"
+	printf "Test 15 - Create Docker Images from Local Dockerfile\n"
+	printf " \n Pres 16 to quit\n"
 
 	printf "\nSelection: "
 	read selection
 
 	case $selection in
-		1  ) 	telnet snf-35274.vm.okeanos-global.grnet.gr 8181
+		1  ) 	telnet snf-35274.vm.okeanos-global.grnet.gr 8181 
 			restart
 			;;
 		2  )   	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/containers | python -mjson.tool
@@ -40,10 +40,10 @@ until [ "$selection" = "0" ]; do
 		3  ) 	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/containers?state=running | python -mjson.tool
 			restart
 			;;
-		4  ) 	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/containers/bfe2305d5b31 | python -mjson.tool
+		4  ) 	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/containers/4458ae876209 | python -mjson.tool
 			restart
 			;;
-		5  ) 	curl -s -X DELETE -H 'Accept: application/json' http://localhost:8181/containers/bfe2305d5b31 | python -mjson.tool
+		5  ) 	curl -s -X DELETE -H 'Accept: application/json' http://localhost:8181/containers/4458ae876209  | python -mjson.tool
 			restart
 			;;
 		6  ) 	curl -s -X DELETE -H 'Accept: application/json' http://localhost:8181/containersDel | python -mjson.tool
@@ -52,25 +52,25 @@ until [ "$selection" = "0" ]; do
 		7  ) 	curl -X POST -H 'Content-Type: application/json' http://localhost:8181/containers -d '{"image": "49b7d316bf66"}' | python -mjson.tool
 			restart
 			;;
-		8  )	curl -X PATCH -H 'Content-Type: application/json' http://localhost:8181/containers/bfe2305d5b31 -d '{"state": "running"}'
+		8  )	curl -X PATCH -H 'Content-Type: application/json' http://localhost:8181/containers/4458ae876209  -d '{"state": "running"}'
 			restart
 			;;
-		9  ) 	curl -X PATCH -H 'Content-Type: application/json' http://localhost:8181/containers/bfe2305d5b31 -d '{"state": "stopped"}'
+		9  ) 	curl -X PATCH -H 'Content-Type: application/json' http://localhost:8181/containers/4458ae876209  -d '{"state": "stopped"}'
 			restart
 			;;
-		10 )	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/containers/bfe2305d5b31/logs | python -mjson.tool
+		10 )	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/containers/4458ae876209 /logs | python -mjson.tool
 			restart
 			;;
 		11 )	curl -s -X GET -H 'Accept: application/json' http://localhost:8181/images | python -mjson.tool
 			restart
 			;;
-		12 )	curl -s -X DELETE -H 'Accept: application/json' http://localhost:8181/images/imgID | python -mjson.tool
+		12 )	curl -s -X DELETE -H 'Accept: application/json' http://localhost:8181/images/07f8e8c5e660 | python -mjson.tool
 			restart
 			;;
 		13 )	curl -s -X DELETE -H 'Accept: application/json' http://localhost:8181/containersDel | python -mjson.tool
 			restart
 			;;
-		14 )	curl -s -X PATCH -H 'Content-Type: application/json' http://localhost:8181/images/id -d '{"tag": "test:1.0"}'
+		14 )	curl -s -X PATCH -H 'Content-Type: application/json' http://localhost:8181/images/07f8e8c5e660 -d '{"tag": "test:1.0"}'
 			restart
 			;;
 		15 )	curl -H 'Accept: application/json' -F file=@Dockerfile http://localhost:8181/images | python -mjson.tool
@@ -78,7 +78,7 @@ until [ "$selection" = "0" ]; do
 			;;
 		99) exit
 			;;
-		* ) printf "You did not choose a valid option!\n"
+		* ) printf "Not a valid option\n"
 	esac
 done
 
